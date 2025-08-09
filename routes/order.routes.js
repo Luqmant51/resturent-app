f544fa5149c4b015c1ecad161ccde0679c08846a
Luqman <luqmant51@gmail.com>
2025-08-09 22:27:40 +0500

    final

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -4,7 +4,7 @@ const orderController = require('../controllers/order.controller');
 
 router.post('/', orderController.createOrder);
 router.get('/orders', orderController.getAllOrders);
-router.post('/orders/:id/confirm', orderController.confirmOrder);
-router.delete('/orders/:id', orderController.deleteOrder);
+router.post('/:id/confirm', orderController.confirmOrder);
+router.delete('/:id', orderController.deleteOrder);
 
 module.exports = router;
